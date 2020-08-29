de096ae2142590d39daf1223223a59c9c4bb1a0c
Bethany Bellio <bethany.wride@gmail.com>
2020-08-28 18:23:40 -0600

    Adds custom CityTVCell

---
+++ WhateverTheWeather/WhateverTheWeather/CityTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CityTableViewCell.swift
+//  WhateverTheWeather
+//
+//  Created by Bethany Wride on 8/28/20.
+//  Copyright © 2020 Bethany Bellio. All rights reserved.
+//
+
+import UIKit
+
+class CityTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
